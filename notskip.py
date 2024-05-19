38292e79ddf554076a0b14971f061af68a3c9b64
Ayush Panchal <113408381+ayushpanchal26@users.noreply.github.com>
2024-05-19 23:47:22 +0530

    notskip.py

---
+++ notskip.py
@@ -0,0 +1,4 @@
+print(f'don;t skip the days of your life so also don't break your streak')
+date = '19-05-2024'
+day = 'sunday'
+print(f' today on {date} and {day} i  was out of my town and far from my desk and workspace so writing these to just don't break my streak')
